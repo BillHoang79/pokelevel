ca12f5fb4b31030f842dbbb734265634d9683aad
bill hoang <billhoang79@gmail.com>
2017-01-28 19:29:58 -0800

    simple pokemon power level thing

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+var userInput = prompt("What's your pokemon power level?")
+
+if(userInput >= 400){
+	alert("Welcome")
+} else {
+	alert("Youre too weak!!")
+}
+
+/**
+function verifyPokemonLevel(){
+	var pokemonPower = Number(prompt("what's your pokemon power level?"))
+
+	if(pokemonPower >= 400){
+		alert("You're strong! Join our Gym")
+	} else {
+		alert("YOU'RE WEAK!! Come back later.")
+	}
+}
+
+verifyPokemonLevel()
+
+*/
